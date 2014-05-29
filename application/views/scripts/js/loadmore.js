91ecf63bae5eb560edc27609a4f9792a738bb63f
Max Bugaenko <max.bugaenko@gmail.com>
2014-05-30 02:46:14 +0300

    full redesign

--- application/views/scripts/js/loadmore.js
+++ application/views/scripts/js/loadmore.js
@@ -2,6 +2,7 @@ function loadMore(insertAfter, url) {
     $.get(url,
         function(data) {
             if (data != "FINISH") {
+                alert(data);
                 $(data).insertAfter(insertAfter);
                 $(window).data("offset", parseInt($(window).data("offset"))+20);
             }
